8478f6c327998ac9705c21b34c1f95100dd8de25
ichahal <aman.0002@icloud.com>
2024-01-24 17:45:36 -0500

    store and customer file transfer

--- Group3_Store/Customer.swift
+++ Group3_Store/Customer.swift
@@ -1,23 +1,8 @@
-// class Customer
+//
+//  Customer.swift
+//  Group3_Store
+//
+//  Created by Aman  Chahal on 1/24/24.
+//
 
-//class Customer {
-//    var itemsList: [OwnedItem]
-//    var balance: Double
-//
-//    init(itemsList: [OwnedItem] = [], balance: Double = 10.0) {
-//        self.itemsList = itemsList
-//        self.balance = balance
-//    }
-//
-//    func reloadAccount(amount: Double) {
-//        balance += amount
-//    }
-//
-//    func useItem(id: Int, numMinutes: Int) {
-//        guard let index = itemsList.firstIndex(where: { $0.item.id == id }) else {
-//            return
-//        }
-//
-//        itemsList[index].useMinutes(numMinutes)
-//    }
-//}
+import Foundation
